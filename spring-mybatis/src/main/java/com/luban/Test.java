0134f995cf4404dc9e0e6b66bb69108adcbdeccf
kzhou <Kaidong.Zhou@eisgroup.com>
2019-10-30 18:25:51 +0800

    V3:Combain Mybatis and Spring Logging

--- spring-mybatis/src/main/java/com/luban/Test.java
+++ spring-mybatis/src/main/java/com/luban/Test.java
@@ -1,13 +1,36 @@
 package com.luban;
 
 import com.luban.config.AppConfig;
+import com.luban.dao.EmployeeDao;
 import com.luban.service.EmployeeService;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.logging.log4j.Log4jImpl;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Test {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        System.out.println(context.getBean(EmployeeService.class).list());
+        /*AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        System.out.println(context.getBean(EmployeeService.class).list());*/
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory.getConfiguration().addMapper(EmployeeDao.class);
+        sqlSessionFactory.getConfiguration().setLogImpl(Log4jImpl.class);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        EmployeeDao employeeDao = sqlSession.getMapper(EmployeeDao.class);
+        System.out.println(employeeDao.list());
     }
 }
--- spring-mybatis/src/main/java/com/luban/config/AppConfig.java
+++ spring-mybatis/src/main/java/com/luban/config/AppConfig.java
@@ -32,6 +32,20 @@ public class AppConfig {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
         org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        /**
+         * spring4 日志 采用原生的jcl log4j>jul
+         * spring5 日志 采用改版的jcl log4j2>slf4j>jul
+         * mybatis 日志 采用自己的日志 slf4j>jcl>log4j2>log4j>jul
+         *
+         * spring5 + mybatis + log4j 只会打印spring日志，不打印mybatis日志，什么原因？
+         *  1.spring5的日志体系自带jcl，而且在没有 log4j2>slf4j 情况下，采用的是jul;
+         *  2.mybatis发现没有slf4j但是有jcl，就会去执行spring的jcl,然后采用了jul;
+         *  3.但是jul的级别是info,而mybatis打印SQL的级别是debug,所以不打印
+         * 如果非要打印呢？
+         * 1. 采用slf4j+slf4j-log4j，强行让两者都打印log4j
+         * 2. 扩展mybatis的日志
+         *
+         */
         configuration.setLogImpl(Log4jImpl.class);
         sqlSessionFactoryBean.setConfiguration(configuration);
         return sqlSessionFactoryBean;
